224e35fd3f67787d459da661d35aba7958073077
Chris King <morza@pop-os.localdomain>
2020-02-13 14:45:01 -0800

    Nodemon watcher

--- watchit/index.js
+++ watchit/index.js
@@ -4,8 +4,41 @@ console.log("I was executed");
 const chokidar = require("chokidar");
 const { spawn } = require("child_process");
 const ls = spawn("ls", ["-lh", "/usr"]);
+const debounce = require("lodash.debounce");
+const program = require("caporal");
+const fs = require("fs");
+const fsPromises = fs.promises;
+const chalk = require('chalk')
 
-// One-liner for current directory
-chokidar.watch(".").on("all", (event, path) => {
-  console.log(event, path);
-});
+//// File watcher
+
+// Caporal
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+
+    try {
+      await fsPromises.access(name);
+    } catch (err) {
+      throw new Error("File doesn't exist");
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc){
+        console.log(chalk.red('<< Ending process'));
+        proc.kill();}
+
+      console.log(chalk.green('>> Starting process'));  
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 200);
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
